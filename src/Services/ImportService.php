bbbc8393a42c743b25ed9a9adbe48712d07af6ae
Ruchit Patel <ruchit.patel@viitor.cloud>
2023-04-06 19:09:14 +0530

    Code style fixing.

--- src/Services/ImportService.php
+++ src/Services/ImportService.php
@@ -10,23 +10,17 @@ use Illuminate\Support\Facades\Schema;
  */
 class ImportService
 {
-    /**
-     * @return Collection
-     */
     public function getTables(): Collection
     {
         $tables = collect();
-        $db = "Tables_in_".env('DB_DATABASE');
+        $db = 'Tables_in_'.env('DB_DATABASE');
         foreach (Schema::getAllTables() as $table) {
             $tables->push($table->{$db});
         }
+
         return $tables;
     }
 
-    /**
-     * @param string $table
-     * @return Collection
-     */
     public function getTableColumns(string $table): Collection
     {
         return collect(Schema::getColumnListing($table))->reject(function (string $value) {
@@ -34,26 +28,22 @@ class ImportService
         });
     }
 
-    /**
-     * @param string $fileName
-     * @return array
-     */
     public function csvToArray(string $fileName): array
     {
         // open csv file
-        if (!($fp = fopen($fileName, 'r'))) {
+        if (! ($fp = fopen($fileName, 'r'))) {
             return [];
         }
 
         //read csv headers
-        $key = fgetcsv($fp,"1024",",");
+        $key = fgetcsv($fp, '1024', ',');
         session(['import.fileColumns' => $key]);
         array_unshift($key, 'Id');
 
         // parse csv rows into array
         $data = [];
         $i = 1;
-        while ($row = fgetcsv($fp,"1024",",")) {
+        while ($row = fgetcsv($fp, '1024', ',')) {
             array_unshift($row, $i);
             $data[] = array_combine($key, $row);
             $i++;
